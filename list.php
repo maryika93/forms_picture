28080692c0b466e22c7f8846554f754b69a1d418
Maria <maryika93@mail.ru>
2018-02-17 12:30:21 +0600

    form_pic

---
+++ list.php
@@ -0,0 +1,15 @@
+
+
+<?php
+$filesPath = '../forms_pictures/tests/';
+$files = scandir($filesPath);
+
+foreach ($files as $value)
+{
+    if ($value !='.' and $value !='..' )
+    {
+        echo '<li><a href="test_2.php?name='. $value.'">'.$value.'</a></li><br>';
+    }
+    else{}
+}
+?>
